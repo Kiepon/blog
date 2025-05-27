a4f2078f3f2281d2846b1e8c7c647df6e472d1f0
kiepon <dmitrii.domnin313@gmail.com>
2025-05-27 16:00:45 +0600

    first commit

---
+++ apps/accounts/forms.py
@@ -0,0 +1,109 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from django_recaptcha.fields import ReCaptchaField
+
+from .models import Profile
+
+
+class UserUpdateForm(forms.ModelForm):
+    """
+    Форма обновления данных пользователя
+    """
+    username = forms.CharField(max_length=100,
+                               widget=forms.TextInput(
+                                   attrs={"class": "form-control mb-1"}
+                               ))
+    email = forms.EmailField(widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
+    first_name = forms.CharField(max_length=100,
+                                 widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
+    last_name = forms.CharField(max_length=100,
+                                widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+        
+    
+    def clean_email(self):
+        """
+        Проверка почты на уникальность
+        """
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError("Email должен быть уникальным")
+        return email
+    
+    
+
+class ProfileUpdateForm(forms.ModelForm):
+    """
+    Форма обновления данных профиля пользователя
+    """
+    birth_date = forms.DateField(
+        widget=forms.TextInput(attrs={"class": "form-control mb-1"})
+    )
+    bio = forms.CharField(max_length=500,
+                          widget=forms.Textarea(attrs={"rows": 5, "class": "form-control mb-1"}))
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={"class": "form-control mb-1"}))
+    
+    
+    class Meta:
+        model = Profile
+        fields = ('birth_date', 'bio', 'avatar')
+        
+        
+    
+class UserRegisterForm(UserCreationForm):
+    """
+    Переопределенная форма регистрации пользователей
+    """
+    class Meta(UserCreationForm.Meta):
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        
+    def clean_email(self):
+        """
+        Проверка почты на уникальность
+        """
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Такой email уже используется в системе")
+        return email
+    
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы регистрации
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': "Придумайте свой логин"})
+        self.fields['password1'].widget.attrs.update({'placeholder': "Придумайте свой пароль"})
+        self.fields['password2'].widget.attrs.update({'placeholder': "Повторите придуманный пароль"})
+        self.fields['email'].widget.attrs.update({'placeholder': "Введите свой email"})
+        self.fields['first_name'].widget.attrs.update({'placeholder': "Ваше имя"})
+        self.fields['last_name'].widget.attrs.update({'placeholder': "Ваше фамилия"})
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control", "autocomplete": "off"})
+            
+
+
+class UserLoginForm(AuthenticationForm):
+    """
+    Форма авторизации на сайте
+    """
+    
+    recaptcha = ReCaptchaField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'recaptcha']
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы авторизации
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Логин пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Пароль пользователя'
+        self.fields['username'].label = 'Логин'
---
+++ blog_cbv/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for blog_cbv project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from apps.blog.feeds import LatestPostFeed
+
+
+handler403 = 'apps.blog.views.tr_handler403'
+handler404 = 'apps.blog.views.tr_handler404'
+handler500 = 'apps.blog.views.tr_handler500'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('feeds/latest/', LatestPostFeed(), name="latest_post_feed"),
+    path('', include("apps.blog.urls")),
+    path('', include("apps.accounts.urls")),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += debug_toolbar_urls()
